610c83f7df823681771fe11da7527cf13896fd5c
micey969 <micey_969@hotmail.com>
2022-10-04 15:27:33 -0400

    Add logout redirect url

--- blogProj/settings.py
+++ blogProj/settings.py
@@ -130,6 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
